56a022404127d4cfc150d87a2d2bacd4d5d66b2e
Ömer Sezer <oomersezer@gmail.com>
2018-02-06 01:04:14 +0300

    Proje githuba atıldı

---
+++ deneme2/deneme2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace deneme2
+{
+    public partial class deneme2Page : ContentPage
+    {
+        public deneme2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
